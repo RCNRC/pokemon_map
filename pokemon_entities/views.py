569bbcd5fae2e30b69a9ae79f1fb4cbad6be1b39
Ilya <Dragoilya@yandex.ru>
2023-02-16 17:42:47 +0300

    Refactored code

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,7 @@
 import folium
 
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
 from pokemon_entities.models import Pokemon, PokemonEntity
 
@@ -59,10 +59,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        pokemon = Pokemon.objects.get(id=pokemon_id)
-    except Pokemon.DoesNotExist:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
 
     local_time = localtime()
     pokemons_entities = PokemonEntity.objects.filter(
